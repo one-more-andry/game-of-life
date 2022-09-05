0987c0e4ecddf3eba5e2142570312f457fcc3514
Andrew Pavlovsky <pavlowskiandrew@gmail.com>
2022-09-05 09:15:21 +0300

    feat: convert input to ArrayList

--- src/main/java/com/lapsho/app/App.java
+++ src/main/java/com/lapsho/app/App.java
@@ -1,13 +1,25 @@
 package com.lapsho.app;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.lang.Integer;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static int[][] getGeneration(int[][] cells, int generations) {
+        List<List<Integer>> cellsList = Arrays.stream(cells)
+                .map(l -> Arrays.stream(l).boxed().collect(Collectors.toList()))
+                .toList();
+
+        return cellsList.stream()
+                .map(l -> l.stream()
+                        .mapToInt(Integer::intValue)
+                        .toArray())
+                .toArray(int[][]::new);
     }
 }
