67d5c8bad177bc2a23cb885ed2ad6d959aa09632
Andrew Pavlovsky <pavlowskiandrew@gmail.com>
2022-09-05 07:19:48 +0300

    feat: updated project java version to 16 & junit to version 5

--- src/test/java/com/lapsho/app/AppTest.java
+++ src/test/java/com/lapsho/app/AppTest.java
@@ -1,8 +1,7 @@
 package com.lapsho.app;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
 
 /**
  * Unit test for simple App.
