259adaa74bbf670e6a9e90586a6f441187f713ec
Andrew Pavlovsky <pavlowskiandrew@gmail.com>
2022-09-06 10:19:17 +0300

    feat: implemented tests

--- src/test/java/com/lapsho/app/AppTest.java
+++ src/test/java/com/lapsho/app/AppTest.java
@@ -9,8 +9,7 @@ import java.util.Arrays;
 public class AppTest 
 {
     @Test
-    public void lifeEmulation_OnValidInput3x3Gen1_ShouldPass()
-    {
+    public void lifeEmulation_OnValidInput3x3Gen1_ShouldPass() {
         int[][] input = {
                 {1,0,0},
                 {0,1,1},
@@ -24,6 +23,97 @@ public class AppTest
         assertTrue(Arrays.deepEquals(expected, output),
                 "Invalid emulation result" + System.lineSeparator() +
                         "Expected: " + System.lineSeparator() + App.htmlize(expected) + System.lineSeparator() +
+                        "Provided: " + System.lineSeparator() + App.htmlize(output));
+    }
+
+    @Test
+    public void lifeEmulation_OneCell_ShouldDie() {
+        int[][] input = {
+                {1,0},
+                {0,0}};
+        int[][] expected = {{}};
+        int[][] output = App.getGeneration(input, 1);
+
+        assertTrue(Arrays.deepEquals(expected, output),
+                "The output should be empty" + System.lineSeparator() +
+                        "Provided: " + System.lineSeparator() + App.htmlize(output));
+    }
+
+    @Test
+    public void lifeEmulation_TwoCells_ShouldLive() {
+        int[][] input = {
+                {1,1},
+                {0,0}};
+        int[][] expected = {
+                {1,1},
+                {0,0}};
+        int[][] output = App.getGeneration(input, 1);
+
+        assertTrue(Arrays.deepEquals(expected, output),
+                "The expected value must be equal to the initial value" + System.lineSeparator() +
+                        "Expected: " + System.lineSeparator() + App.htmlize(expected) + System.lineSeparator() +
+                        "Provided: " + System.lineSeparator() + App.htmlize(output));
+    }
+
+    @Test
+    public void lifeEmulation_ThreeCells_ShouldProduceExtraOne() {
+        int[][] input = {
+                {0,1},
+                {1,1}};
+        int[][] expected = {
+                {1,1},
+                {1,1}};
+        int[][] output = App.getGeneration(input, 1);
+
+        assertTrue(Arrays.deepEquals(expected, output),
+                "Invalid emulation" + System.lineSeparator() +
+                        "Expected: " + System.lineSeparator() + App.htmlize(expected) + System.lineSeparator() +
+                        "Provided: " + System.lineSeparator() + App.htmlize(output));
+    }
+
+    @Test
+    public void lifeEmulation_CellExpansion_ShouldExtendArray() {
+        int[][] input = {
+                {1,1,1},
+                {0,0,0},
+                {0,0,0}};
+        int[][] expected = {
+                {0,1,0},
+                {1,1,1},
+                {0,1,0}};
+        int[][] output = App.getGeneration(input, 1);
+
+        assertTrue(Arrays.deepEquals(expected, output),
+                "Invalid emulation" + System.lineSeparator() +
+                        "Expected: " + System.lineSeparator() + App.htmlize(expected) + System.lineSeparator() +
+                        "Provided: " + System.lineSeparator() + App.htmlize(output));
+    }
+
+    @Test
+    public void lifeEmulation_DeadCells_ShouldBeCropped() {
+        int[][] input = {
+                {1,0,0},
+                {1,0,0},
+                {0,0,0}};
+        int[][] expected = {
+                {1,0},
+                {1,0}};
+        int[][] output = App.getGeneration(input, 1);
+
+        assertTrue(Arrays.deepEquals(expected, output),
+                "Invalid emulation" + System.lineSeparator() +
+                        "Expected: " + System.lineSeparator() + App.htmlize(expected) + System.lineSeparator() +
+                        "Provided: " + System.lineSeparator() + App.htmlize(output));
+    }
+
+    @Test
+    public void lifeEmulation_NoLivingCells_EmptyArrayShouldBeReturned() {
+        int[][] input = {{}};
+        int[][] expected = {{}};
+        int[][] output = App.getGeneration(input, 1);
+
+        assertTrue(Arrays.deepEquals(expected, output),
+                "Empty array expected" + System.lineSeparator() +
                         "Provided: " + System.lineSeparator() + App.htmlize(output));
     }
 }
